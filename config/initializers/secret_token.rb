9a6d9c4087cb258ffbc5d3625aa83be4f221d850
qleech <qleech@yandex.ru>
2014-02-18 20:31:02 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Autoshop::Application.config.secret_key_base = '142507568204900c71a789c03b87a8b6c908527ed17af15a304c0cc6c2df2b324c61973a9fd5fa566b1bbc9a84910ae27f3027b22257cee4fb85883bb9f45cf2'
